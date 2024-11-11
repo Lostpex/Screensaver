15004f554f831faff56d0b7eaa78ee773928b128
Hagen <hagenotto23@icloud.com>
2024-11-11 22:21:12 +0100

    first test

---
+++ script.js
@@ -0,0 +1,29 @@
+const apiKey = '-E7nWnmsIUhhtkocOf2PCbHNyKAR_6Z2BoXnlHLK5hk';
+const imageElement = document.getElementById('slideshow-image');
+
+async function fetchRandomImage() {
+  try {
+    const response = await fetch(`https://api.unsplash.com/photos/random?client_id=${apiKey}`);
+    const data = await response.json();
+    return data.urls.regular;
+  } catch (error) {
+    console.error("Fehler beim Laden des Bildes:", error);
+    return null;
+  }
+}
+
+async function startSlideshow() {
+  while (true) {
+    const imageUrl = await fetchRandomImage();
+    if (imageUrl) {
+      imageElement.src = imageUrl;
+      imageElement.style.opacity = 1;
+      await setTimeout(1000)
+      await new Promise(resolve => setTimeout(resolve, 10000)); // 5 Sekunden pro Bild
+      imageElement.style.opacity = 0;
+      await new Promise(resolve => setTimeout(resolve, 15000)); // 
+    }
+  }
+}
+
+startSlideshow();
